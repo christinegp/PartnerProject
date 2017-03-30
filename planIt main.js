660bc9c0b0da8ff7043928e4c70b0beb6050846b
Christine <christinegp@users.noreply.github.com>
2017-03-29 17:58:49 -0700

    planIt main.js
    new JS for planIT

---
+++ planIt main.js
@@ -0,0 +1,231 @@
+function SetDate(){
+  var date = new Date();
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
+  if (month < 10) month = "0" + month;
+  if (day < 10) day = "0" + day;
+  var today = year + "-" + month + "-" + day;
+  document.getElementById('myDate').value = today;
+  }
+//eventful
+function show_alert2(){
+     var oArgs = {
+        app_key: "qtzhbLdNnWNtZL7z",
+        q: "music",
+        where: "Los Angeles",
+        "date": "Future",
+        page_size: 5,
+        sort_order: "popularity",
+     };
+     EVDB.API.call("/events/search", oArgs, function(oData) {
+       console.log(oData);
+      var eventsData = JSON.stringify(oData);
+      $('#events').text(eventsData);
+      });
+  }
+  $('#GetEvents').click(function(){
+    show_alert2();
+  })
+
+//This is the api for eventful-- select id="activities"
+// $("#myBtn").click(function(){
+// 	$('#events').append('Your Events For the Day Go HERE');
+// });
+
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//google maps 1
+// function initMap() {
+//     var map = new google.maps.Map(document.getElementById('map'), {
+//       center: {lat: -33.8688, lng: 151.2195},
+//       zoom: 13
+//     });
+//     var input = document.getElementById('searchInput');
+//     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+//
+//     var autocomplete = new google.maps.places.Autocomplete(input);
+//     autocomplete.bindTo('bounds', map);
+//
+//     var infowindow = new google.maps.InfoWindow();
+//     var marker = new google.maps.Marker({
+//         map: map,
+//         anchorPoint: new google.maps.Point(0, -29)
+//     });
+//
+//     autocomplete.addListener('place_changed', function() {
+//         infowindow.close();
+//         marker.setVisible(false);
+//         var place = autocomplete.getPlace();
+//         if (!place.geometry) {
+//             window.alert("Autocomplete's returned place contains no geometry");
+//             return;
+//         }
+//
+//         // If the place has a geometry, then present it on a map.
+//         if (place.geometry.viewport) {
+//             map.fitBounds(place.geometry.viewport);
+//         } else {
+//             map.setCenter(place.geometry.location);
+//             map.setZoom(17);
+//         }
+//         marker.setIcon(({
+//             url: place.icon,
+//             size: new google.maps.Size(71, 71),
+//             origin: new google.maps.Point(0, 0),
+//             anchor: new google.maps.Point(17, 34),
+//             scaledSize: new google.maps.Size(35, 35)
+//         }));
+//         marker.setPosition(place.geometry.location);
+//         marker.setVisible(true);
+//
+//         var address = '';
+//         if (place.address_components) {
+//             address = [
+//               (place.address_components[0] && place.address_components[0].short_name || ''),
+//               (place.address_components[1] && place.address_components[1].short_name || ''),
+//               (place.address_components[2] && place.address_components[2].short_name || '')
+//             ].join(' ');
+//         }
+//
+//         infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+//         infowindow.open(map, marker);
+//
+//         //Location details
+//         for (var i = 0; i < place.address_components.length; i++) {
+//             if(place.address_components[i].types[0] == 'postal_code'){
+//                 document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
+//             }
+//             if(place.address_components[i].types[0] == 'country'){
+//                 document.getElementById('country').innerHTML = place.address_components[i].long_name;
+//             }
+//         }
+//         document.getElementById('location').innerHTML = place.formatted_address;
+//         document.getElementById('lat').innerHTML = place.geometry.location.lat();
+//         document.getElementById('lon').innerHTML = place.geometry.location.lng();
+//     });
+// }
+
+var map;
+var infowindow;
+
+var request;
+var service;
+var markers = [];
+
+function initialize() {
+  var center = new google.maps.LatLng(34.0432782,-118.2850588);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: center,
+    zoom: 13
+  });
+
+  request = {
+    location: center,
+    radius: 8047,
+    types: ['cafe','bakery','restaurant','bar']
+  };
+  infowindow = new google.maps.InfoWindow();
+
+  service = new google.maps.places.PlacesService(map);
+
+  service.nearbySearch(request, callback);
+
+  google.maps.event.addListener(map,'rightclick', function(event){
+    map.setCenter(event.latLng)
+    clearResults(markers)
+
+    var request = {
+      location: event.latLng,
+      radius: 8047,
+      types: ['cafe']
+    };
+    service.nearbySearch(request, callback);
+  })
+}
+function callback(results,status){
+  if(status == google.maps.places.PlacesServiceStatus.OK){
+    for(var i = 0; i < results.length; i++){
+      markers.push(createMarker(results[i]));
+    }
+  }
+}
+function createMarker(place){
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker,'click', function(){
+    infowindow.setContent(place.name);
+    infowindow.open(map,this);
+  });
+  return marker;
+
+}
+
+  function clearResults(markers){
+    for (var m in markers) {
+      markers[m].setMap(null)
+    }
+    markers = []
+  }
+  
+google.maps.event.addDomListener(window,'load',initialize);
+
+
+
+//
+//
+////
+////
+////
+//		var apiKey = 'qtzhbLdNnWNtZL7z',
+//			zipCode = $('#txt-zip').val(),
+//			$resultsArea = $('#rep-lookup-results');
+//		
+//		var requestURL = 'http://api.eventful.com/js/api?callback=?';
+//
+//		$.getJSON(requestURL, {
+//			'apikey' : apiKey,
+//			'url' : link,
+//		}, function(data){
+//			console.log(data);
+//			
+//			if (data.results && data.results.length > 0) {
+//			
+//				var mySenators = '<p>Here are your Senators:</p>';
+//			
+//				$.each(data.results, function(i, rep) {
+//					if ('senate' === rep.chamber.toLowerCase()) {
+//						mySenators += '<p>';
+//						mySenators += '<a href="' + rep.contact_form + '" target="_blank">';
+//						mySenators += rep.first_name + ' ' + rep.last_name;
+//						mySenators += '</a>';
+//						mySenators += '</p>';
+//					}
+//				});
+//			
+////                
+////      'url': link
